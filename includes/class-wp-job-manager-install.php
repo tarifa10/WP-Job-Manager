8ced3ac0e2c1e4b94b8a64276ea2fcab30b88635
Jake Oehler Morrison <jake.morrison@automattic.com>
2018-12-07 14:12:58 +0000

    Introduce admin notice helper class along with setup notice

--- includes/class-wp-job-manager-install.php
+++ includes/class-wp-job-manager-install.php
@@ -21,9 +21,10 @@ class WP_Job_Manager_Install {
 		self::init_user_roles();
 		self::default_terms();
 
-		// Redirect to setup screen for new installs.
+		// Fresh installs should be prompted to set up their instance.
 		if ( ! get_option( 'wp_job_manager_version' ) ) {
-			set_transient( '_job_manager_activation_redirect', 1, HOUR_IN_SECONDS );
+			include_once JOB_MANAGER_PLUGIN_DIR . '/includes/admin/class-wp-job-manager-admin-notices.php';
+			WP_Job_Manager_Admin_Notices::add_notice( WP_Job_Manager_Admin_Notices::NOTICE_CORE_SETUP );
 		}
 
 		// Update featured posts ordering.
